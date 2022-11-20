fa56ea8c2ca822cda06bd27ce673fd3b1736a592
Slavy Spasoff <slavyspasoff@gmail.com>
2022-11-20 17:57:05 +0100

    add guest model validaton

--- src/models/guest.model.ts
+++ src/models/guest.model.ts
@@ -18,15 +18,31 @@ const guestSchema = new Schema<
   {
     name: {
       type: String,
+      required: [true, 'Guest must have a name.'],
+      minlength: [6, 'Name must be between 6 and 100 characters long.'],
+      maxlength: [100, 'Name must be between 6 and 100 characters long.'],
     },
     email: {
       type: String,
+      required: [true, 'Guest must have an email.'],
+      maxlength: [100, 'Email must be no more than 100 characters long.'],
+      unique: true,
     },
     password: {
       type: String,
+      required: [true, 'Guest must have a password.'],
+      minlength: [8, 'Password must be between 8 and 50 characters long.'],
+      maxlength: [50, 'Password must be between 8 and 50 characters long.'],
     },
     confirmationPassword: {
       type: String,
+      required: [true, 'Guest must have a confirmation password.'],
+      validate: {
+        validator: function (this: GuestDocument, v: string) {
+          return v === this.password;
+        },
+        message: 'Password and confirmation password are not the same.',
+      },
     },
     bookings: [
       {
